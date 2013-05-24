67668abcc17706cefc65af18bc3caea60c7de7c1
Emanuele Rudel <erudel@student.ethz.ch>
2013-05-24 15:35:49 +0200

    - Changed game name

--- WindowsGame2/WindowsGame2/WindowsGame2/Properties/AssemblyInfo.cs
+++ WindowsGame2/WindowsGame2/WindowsGame2/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsGame2")]
-[assembly: AssemblyProduct("WindowsGame2")]
+[assembly: AssemblyTitle("The Drunken Dream Maker (With a Cold)")]
+[assembly: AssemblyProduct("The Drunken Dream Maker (With a Cold)")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Triolozzi")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
